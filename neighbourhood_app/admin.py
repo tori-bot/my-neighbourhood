7c47764115e1e1e7e2856c96b56689ee9492b404
victoria makena <makenavictoria1@gmail.com>
2022-06-19 23:23:44 +0300

    admin: register models in admin

--- neighbourhood_app/admin.py
+++ neighbourhood_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import User,Profile,Neighborhood,Business,Admin
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Profile)
+admin.site.register(Business)
+admin.site.register(Neighborhood)
+admin.site.register(Admin)
--- neighbourhood_app/models.py
+++ neighbourhood_app/models.py
@@ -13,6 +13,7 @@ from django.dispatch import receiver
 class Admin(models.Model):
     name = models.CharField(max_length=50)
     email = models.EmailField(max_length=300)
+    neighborhood_admin = models.CharField(max_length=300,null=True)
 
     def __str__(self):
         return self.name

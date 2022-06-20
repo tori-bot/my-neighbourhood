d7d86173b26c7c7f704a8c1024b5e91e23fc4a9b
victoria makena <makenavictoria1@gmail.com>
2022-06-20 13:21:11 +0300

    profile: configure profile urls

--- neighbourhood_app/urls.py
+++ neighbourhood_app/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('',views.home,name='home'),
+
+    path('profile_form/<int:id>/', views.profile_form, name='profile_form'),
+    path('profile/', views.profile, name='profile'),
+    path('user_profile/<int:id>/',
+         views.user_profile, name='user_profile'),
 ]

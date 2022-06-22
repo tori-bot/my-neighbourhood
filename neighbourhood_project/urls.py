4826cba4ec3c6733726146032cd440a0b292278e
victoria makena <makenavictoria1@gmail.com>
2022-06-22 14:03:49 +0300

    fix bugs and deploy

--- neighbourhood_project/urls.py
+++ neighbourhood_project/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path,include
 from django.contrib.auth import views
 from django_registration.backends.one_step.views import RegistrationView
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,3 +29,6 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('logout/', views.logout_then_login, name='logout')
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
